b529418a1dc8d249be5b92ccf64124b310ac7d67
大樹 髙橋 <190527@jc-21.jp>
2021-12-03 14:51:15 +0900

    変更

--- R03BMI/MainPage.xaml.cs
+++ R03BMI/MainPage.xaml.cs
@@ -18,7 +18,86 @@ namespace R03BMI
 
         private void Button_Clicked(object sender, EventArgs e)
         {
+            try
+{
+            double height = double.Parse(heighit.Text);
+            double weight = double.Parse(weighit.Text);
+            string sin = "m";
+            string tai = "kg";
+            int flag1 = 0;
+            int flag2 = 0;
+            if (height > 10)
+{
+height = height / 100;
 
+
+
+            flag1 = 1;
+               sin = "cm";
+
+}
+            if (weight > 1000)
+{
+               weight = weight / 1000;
+               flag2 = 1;
+               tai = "g";
+
+}
+               double BMI = Math.Round(weight / (height * height),1);
+
+
+
+            string i;
+            if (BMI < 18.5)
+{
+i = "低体重";
+}
+else if (BMI < 25)
+{
+i = "普通体重";
+}
+else if (BMI<30)
+{
+i = "肥満(1度)";
+}
+else if (BMI < 35)
+{
+i = "肥満(2度)";
+}
+else if (BMI < 40)
+{
+i = "肥満(3度)";
+}
+else
+{
+i = "肥満(4度)";
+}
+if(flag1>0)
+{
+height = height* 100;
+}
+if(flag2>0)
+{
+weight = weight * 1000;
+}
+
+
+
+
+//身長1.6m　体重４６ｋｇのＢＭＩは？？？肥満１です。
+result.Text ="身長"+height+sin+"体重"+weight+tai+"のBMIは"+BMI+i+"です";
+}
+catch (FormatException ex)
+
+{
+Console.WriteLine(ex);
+result.Text = "!!!";
+
+
+
+
+            }
         }
     }
 }
+
